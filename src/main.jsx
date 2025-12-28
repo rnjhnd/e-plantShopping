e6a655e1b244b98357aa2fa7a32b4747b099dc36
Aren John Esoen <esoen.arenjohn@gmail.com>
2025-12-28 08:18:18 -0500

    Completed project tasks: Redux store and logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux'; // Import Provider
+import store from './store.js'; // Import the configured store
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap App with Provider and pass the store prop */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-)
+);
